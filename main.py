1d45cca239ea912e3e3d5ace3971631f1000a62d
00014555 <wiut00014555@gmail.com>
2021-11-05 15:35:24 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# eng2uz = {'one': 'bir', 'two': 'ikki', 'three': 'uch'}
+# print(eng2uz['two'])
+#
+# eng2uz = {'one': 'bir', 'two': 'ikki', 'three': 'uch'}
+# print(len(eng2uz))
+#
+#
+# eng2uz = {'one': 'bir', 'two': 'ikki', 'three': 'uch'}
+# print('one' in eng2uz )
+#
+# eng2uz = {'one': 'bir', 'two': 'ikki', 'three': 'uch'}
+# vals = eng2uz.values()
+# print('bir' in vals)
+
+
+#
+# def histogram(s):
+#     d = dict()
+#     for c in s:
+#         if c not in d:
+#            d[c] = 1
+#         else:
+#            d[c] += 1
+#      return d
+
+
+#     h = histogram('a')
+#     h
+# {'a': 1}
+#     h.get('a', 0)
+# 1
+#     h.get('c', 0)
+# 0
